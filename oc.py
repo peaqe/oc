2ac1b1f4c81a59ef75a1318fa79e227117c29c62
Rudá Moura <rmoura@redhat.com>
2019-07-16 17:57:48 -0300

    Triple quotes for docstrings + cosmetic fixes

--- oc.py
+++ oc.py
@@ -8,7 +8,10 @@ logger = logging.getLogger(__name__)
 
 
 def oc(params):
-    "Run oc with params. May raise 'RuntimeError' on error."
+    """Run oc with params.
+
+    May raise `RuntimeError` on error.
+    """
     cmd = f'oc {params}'
     logger.debug(cmd)
     output = pexpect.run(cmd).decode('utf-8')
@@ -19,56 +22,59 @@ def oc(params):
 
 
 def parse(output):
-    "Parse (normalize) output and return a list of lines."
+    """Parse (normalize) output and return a list of lines."""
     output = output.split('\r\n')
     output = [line for line in output if line]
     return output
 
 
 def login(openshift_url, token):
-    "Log in on openshift_url with Bearer token."
+    """Log in on openshift_url with Bearer token."""
     cmd = f'login {openshift_url} --token={token}'
     return parse(oc(cmd))
 
 
 def whoami():
-    "Get my username."
+    """Get my username."""
     cmd = f'whoami'
     return parse(oc(cmd))
 
 
 def logout():
-    "Log out from OpenShift."
+    """Log out from OpenShift."""
     cmd = 'logout'
     return parse(oc(cmd))
 
 
 def get_projects():
-    "Get all projects."
+    """Get all projects."""
     cmd = 'projects -q'
     return parse(oc(cmd))
 
 
 def get_project():
-    "Get current project."
+    """Get current project."""
     cmd = 'project'
     return parse(oc(cmd))
 
 
 def set_project(name):
-    "Select project by name."
+    """Select project by name."""
     cmd = f'project {name}'
     return parse(oc(cmd))
 
 
 def get_pods():
-    "Get all pods."
+    """Get all pods."""
     cmd = 'get pods'
     return parse(oc(cmd))
 
 
 def logs(pod, since=None, tail=None, timestamps=None):
-    "Get logs from pod. Provides flags for the `since`, `tail`, and `timestamps` flags."
+    """Get logs from pod.
+
+    Provides flags for the `since`, `tail`, and `timestamps` flags.
+    """
     cmd = f'logs {pod}'
     if since:
         cmd = cmd + f' --since={since}'
@@ -80,12 +86,12 @@ def logs(pod, since=None, tail=None, timestamps=None):
 
 
 def exec(pod, command):
-    "Execute command in pod."
+    """Execute command in pod."""
     cmd = f'exec {pod} -- {command}'
     return parse(oc(cmd))
 
 
 def rsh(pod, command):
-    "Open remote shell in pod and execute a command."
+    """Open remote shell in pod and execute a command."""
     cmd = f'rsh {pod} {command}'
     return parse(oc(cmd))
