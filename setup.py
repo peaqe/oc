dfefe700fbf7375bc3e7d31fb7604c8ffbe1ed07
Rudá Moura <rmoura@redhat.com>
2019-07-26 12:13:09 -0300

    Update documentation

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get long description from README file
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    readme = f.read()
 
 setup(
     name='oc',
     version='0.2.0',
-    description='OpenShift CLI (oc) thin wrapper',
+    license='Apache License',
+    description='OpenShift CLI (oc) thin wrapper library for Python3',
+    long_description=readme,
     author='Rudá Moura',
     author_email="rmoura@redhat.com",
     packages=find_packages(include=['oc']),
@@ -14,4 +24,5 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Topic :: Software Development :: Libraries',
     ],
+    url='https://github.com/peaqe/oc/',
 )
